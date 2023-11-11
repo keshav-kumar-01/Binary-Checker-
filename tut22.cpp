885facb6881da9063b13122a9aff80edc114dc63
keshav kumar <76901651+keshav-kumar-01@users.noreply.github.com>
2023-11-11 10:50:39 +0530

    initial commit

---
+++ tut22.cpp
@@ -0,0 +1,63 @@
+#include <iostream>
+#include <string.h>
+using namespace std;
+
+class binary
+{
+    string s;
+
+public:
+    void read(void);
+    void chk_bin(void);
+    void ones(void);
+    void display(void);
+};
+
+void binary ::read(void)
+{
+    cout << "Enter the binary Number" << endl;
+    cin >> s;
+}
+void binary ::chk_bin(void)
+{
+    for (int i = 0; i < s.length(); i++)
+    {
+        if (s.at(i) != '0' && s.at(i) != '1')
+        {
+            cout << "This is not a binary " << endl;
+            exit(0);
+        }
+    }
+}
+
+void binary ::ones(void)
+{
+    chk_bin();
+    for (int i = 0; i < s.length(); i++)
+    {
+        if (s.at(i) == '0')
+        {
+            s.at(i) = '1';
+        }
+        else
+        {
+            s.at(i) = '0';
+        }
+    }
+}
+void binary ::display(void)
+{
+    for (int i = 0; i < s.length(); i++)
+    {
+        cout << s.at(i);
+    }
+}
+int main()
+{
+    binary b;
+    b.read();
+    // b.chk_bin();
+    b.ones();
+    b.display();
+    return 0;
+}
